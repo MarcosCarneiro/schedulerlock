d9d6b2d00bc7707b79fbf080e9974fc28abfeb92
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2023-10-21 17:05:59 -0300

    feat: add application.properties configs too

--- src/main/java/com/marcoscl/schedulerlock/SchedulerlockApplication.java
+++ src/main/java/com/marcoscl/schedulerlock/SchedulerlockApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
+@EnableSchedulerLock(defaultLockAtMostFor = "PT60S")
 @SpringBootApplication
 public class SchedulerlockApplication {
 
